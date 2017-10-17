a6f36d0502e830f462b85115680b5492c0acab77
Rahul B <rahulbot@gmail.com>
2017-10-17 13:36:33 -0400

    try to handle slider events better

--- client/app/components/Settings/Settings.js
+++ client/app/components/Settings/Settings.js
@@ -36,6 +36,7 @@ class Settings extends Component {
 
     handleChange(e, key, is_bool, is_dual_slider) {
         const new_settings = this.state.settings;
+        e.preventDefault();
         if (is_dual_slider){
             new_settings[key+'_min'] = e[0];
             new_settings[key+'_max'] = e[1];
