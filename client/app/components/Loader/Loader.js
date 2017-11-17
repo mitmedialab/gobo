50e6286259a7709389311dd46bbe0b035a988d8b
Rahul B <rahulbot@gmail.com>
2017-11-17 09:52:00 -0500

    Clean up Loader code

--- client/app/components/Loader/Loader.js
+++ client/app/components/Loader/Loader.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const Loader = function() {
-    return (
-        <div className="loader">
-            <div className="spinner">
-                <div className="bounce1"></div>
-                <div className="bounce2"></div>
-                <div className="bounce3"></div>
-            </div>
-        </div>
-    );
-};
+const Loader = () => (
+	<div className="loader">
+		<div className="spinner">
+			<div className="bounce1" />
+			<div className="bounce2" />
+			<div className="bounce3" />
+		</div>
+	</div>
+);
 
 export default Loader;
