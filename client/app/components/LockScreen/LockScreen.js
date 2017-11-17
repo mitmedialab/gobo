14254842e23e7b66ffdf295325f74c7e6b3c1769
Rahul B <rahulbot@gmail.com>
2017-11-17 09:54:04 -0500

    clean up lock screen code

--- client/app/components/LockScreen/LockScreen.js
+++ client/app/components/LockScreen/LockScreen.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 import Loader from 'components/Loader/Loader';
 
@@ -10,75 +9,73 @@ import { verifyBetaPassword } from 'actions/passwordLock';
 
 
 const propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    passwordLock: PropTypes.object.isRequired,
+	dispatch: PropTypes.func.isRequired,
+	passwordLock: PropTypes.object.isRequired,
 };
 
 class LockScreen extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            passwordValue: "",
-            focus:false,
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			passwordValue: '',
+			focus: false,
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
 
-    handleChange(event) {
-        this.setState({
-            passwordValue: event.target.value,
-        });
-    }
+	onSubmit() {
+		this.props.dispatch(verifyBetaPassword(this.state.passwordValue));
+	}
 
-    onSubmit() {
-        this.props.dispatch(verifyBetaPassword(this.state.passwordValue))
-    }
+	handleChange(event) {
+		this.setState({
+			passwordValue: event.target.value,
+		});
+	}
 
+	render() {
+		return (
+			<div className="create_account_screen">
+				<h1>Registration</h1>
+				<div className="create_account_form">
+					<div className="content">
+						<div>
+							<p>
+								Gobo is currently testing with a select group of people (we called this a &ldquo;closed beta&rdquo;).
+								<br />
+								If you have the access code that lets you create an account, enter it below and then you can register.
+								<br />
+								If you don&apos;t have the access code, check back in early November!
+							</p>
 
+							<Input
+								text="Beta Password"
+								type="password"
+								ref="beta-password"
+								minCharacters="0"
+								requireCapitals="0"
+								requireNumbers="0"
+								value={this.state.passwordValue}
+								emptyMessage="Password can't be empty"
+								onChange={this.handleChange}
+							/>
 
-    render() {
-        return (
-            <div className="create_account_screen">
-                <h1>Registration</h1>
-                <div className="create_account_form">
-                    <div className="content">
-                        <div>
-                            <p>
-                                Gobo is currently testing with a select group of people (we called this a "closed beta").
-                                <br/>
-                                If you have the access code that lets you create an account, enter it below and then you can register.
-                                <br/>
-                                If you don't have the access code, check back in early November!
-                            </p>
+							<button className="button button_wide" onClick={this.onSubmit}>Register</button>
 
-                            <Input
-                                text="Beta Password"
-                                type="password"
-                                ref="beta-password"
-                                minCharacters="0"
-                                requireCapitals="0"
-                                requireNumbers="0"
-                                value={this.state.passwordValue}
-                                emptyMessage="Password can't be empty"
-                                onChange={this.handleChange}
-                            />
+							{this.props.passwordLock.checking_password && <Loader />}
 
-                                <button className="button button_wide" onClick={this.onSubmit}>Register</button>
+							<div className="status-text"> {this.props.passwordLock.pw_error_text} </div>
 
-
-                                {this.props.passwordLock.checking_password && <Loader/>}
-
-                                <div className="status-text"> {this.props.passwordLock.pw_error_text} </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 LockScreen.propTypes = propTypes;
+
 export default connect(state => ({ passwordLock: state.passwordLock }))(LockScreen);
