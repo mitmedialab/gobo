35e0deebf6f5e008ff602331faf6f47f4f33befd
Rahul B <rahulbot@gmail.com>
2017-11-22 13:28:20 -0500

    Linting cleanup on registration components

--- client/app/components/Register/Register.js
+++ client/app/components/Register/Register.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 
 import CouhesMessage from './registerSteps/CouhesMessage';
@@ -10,8 +9,6 @@ import SelectPersona from './registerSteps/SelectPersona';
 import RegisterSuccess from './registerSteps/RegisterSuccess';
 
 const propTypes = {
-	dispatch: PropTypes.func.isRequired,
-	auth: PropTypes.object.isRequired,
 	passwordLock: PropTypes.object.isRequired,
 };
 
@@ -19,45 +16,42 @@ class Register extends Component {
 
 	constructor(props) {
 		super(props);
-		var step = 1
+		let step = 1;
 		if (props.passwordLock.signup_step > step) {
-			step = props.passwordLock.signup_step
+			step = props.passwordLock.signup_step;
 		}
-		this.state = {
-			step: step,
-		};
+		this.state = { step };
 		this.nextStep = this.nextStep.bind(this);
 		this.prevStep = this.prevStep.bind(this);
 	}
-	nextStep() {
-		this.setState({
-			step : this.state.step + 1
-		})
-	}
-
-	prevStep() {
-		this.setState({
-			step : this.state.step - 1
-		})
-	}
 
 	getCurrentElement() {
-		switch(this.state.step) {
-			case 1:
-				return <CouhesMessage onFinish={this.nextStep}/>
-			case 2:
-				return <SignupEmailPassword onFinish={this.nextStep}/>
-			case 3:
-				return <SocialAuth onFinish={this.nextStep}/>
-			case 4:
-				return <SelectPersona onFinish={this.nextStep}/>
-			case 5:
-				return <RegisterSuccess />
+		switch (this.state.step) {
+		case 1:
+			return <CouhesMessage onFinish={this.nextStep} />;
+		case 2:
+			return <SignupEmailPassword onFinish={this.nextStep} />;
+		case 3:
+			return <SocialAuth onFinish={this.nextStep} />;
+		case 4:
+			return <SelectPersona onFinish={this.nextStep} />;
+		case 5:
+			return <RegisterSuccess />;
+		default:
+			return <CouhesMessage onFinish={this.nextStep} />;
 		}
 	}
 
+	nextStep() {
+		this.setState({ step: this.state.step + 1 });
+	}
+
+	prevStep() {
+		this.setState({ step: this.state.step - 1 });
+	}
+
 	render() {
-		let element = this.getCurrentElement();
+		const element = this.getCurrentElement();
 
 		return (
 			<div className="create_account_screen">
@@ -67,8 +61,8 @@ class Register extends Component {
 						{element}
 
 						<div className="next-back-buttons">
-							{(this.state.step>3 || this.state.step==2) && <button onClick={this.prevStep} >Back</button>}
-							{this.state.step>2 && this.state.step<4 && <button onClick={this.nextStep} >Next</button>}
+							{(this.state.step > 3 || this.state.step === 2) && <button onClick={this.prevStep} >Back</button>}
+							{this.state.step > 2 && this.state.step < 4 && <button onClick={this.nextStep} >Next</button>}
 						</div>
 					</div>
 				</div>
@@ -78,4 +72,5 @@ class Register extends Component {
 }
 
 Register.propTypes = propTypes;
-export default connect(state => ({ auth: state.auth, passwordLock: state.passwordLock}))(Register);
+
+export default connect(state => ({ auth: state.auth, passwordLock: state.passwordLock }))(Register);
--- client/app/components/RegisterWrapper/RegisterWrapper.js
+++ client/app/components/RegisterWrapper/RegisterWrapper.js
@@ -6,36 +6,35 @@ import { getLockStatus } from 'actions/passwordLock';
 import Register from 'components/Register/Register';
 import LockScreen from 'components/LockScreen/LockScreen';
 
-
 const propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    passwordLock: PropTypes.object.isRequired,
+	dispatch: PropTypes.func.isRequired,
+	passwordLock: PropTypes.object.isRequired,
 };
+
 class RegisterWrapper extends Component {
-    componentWillMount() {
-        this.props.dispatch(getLockStatus());
-    }
+	componentWillMount() {
+		this.props.dispatch(getLockStatus());
+	}
 
-    render() {
-        let element;
-        console.log(this.props.passwordLock)
-        if (this.props.passwordLock.password_verified || !this.props.passwordLock.is_locked ||
-            this.props.passwordLock.signup_step>-1 ) {
-                element = <Register/>
-        }
-        else {
-                element = <LockScreen/>
-
-        }
-        return element
-    }
+	render() {
+		let element;
+		if (this.props.passwordLock.password_verified || !this.props.passwordLock.is_locked ||
+			this.props.passwordLock.signup_step > -1) {
+			element = <Register />;
+		} else {
+			element = <LockScreen />;
+		}
+		return element;
+	}
 
 }
 
-function mapStateToProps(state) {
-    return {
-        passwordLock: state.passwordLock,
-    };
-}
+const mapStateToProps = (state) => {
+	return {
+		passwordLock: state.passwordLock,
+	};
+};
+
 RegisterWrapper.propTypes = propTypes;
+
 export default connect(mapStateToProps)(RegisterWrapper);
