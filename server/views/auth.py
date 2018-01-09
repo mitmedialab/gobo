2f3b9bf2848a7f634db11db625995a2c7c4cea3e
sarahkemi <sarahaladetan@gmail.com>
2018-01-09 18:28:42 -0500

    first go at account deletion endpoint

--- server/views/auth.py
+++ server/views/auth.py
@@ -4,7 +4,7 @@ from flask_login import login_required, login_user, logout_user, current_user
 from sqlalchemy.exc import IntegrityError
 
 from server.core import db, bcrypt
-from server.models import User
+from server.models import User, Post, SettingsUpdate, Settings, TwitterAuth, FacebookAuth, post_associations_table
 from server.blueprints import api
 
 logger = logging.getLogger(__name__)
@@ -55,6 +55,39 @@ def login():
     response = jsonify({'result': status, 'user':user_result})
     return response
 
+@api.route ('/delete_acct', methods=['GET'])
+@login_required
+def delete_account():
+    user_id = current_user.id
+    try:
+        #delete posts with post_associations matching user_id
+        post_assocs = db.session.query(post_associations_table).filter(post_associations_table.c.user_id==user_id)
+        post_ids = [post_id for (user_id,post_id) in post_assocs.all()]   
+        post_assocs.delete(synchronize_session=False)
+
+        for pid in post_ids:
+            db.session.query(Post).filter(Post.id==pid).delete()
+
+        #delete user info from other tables
+        db.session.query(FacebookAuth).filter(FacebookAuth.user_id==user_id).delete()
+        db.session.query(TwitterAuth).filter(TwitterAuth.user_id==user_id).delete()
+        db.session.query(SettingsUpdate).filter(SettingsUpdate.user_id==user_id).delete()
+        db.session.query(Settings).filter(Settings.user_id==user_id).delete()
+
+        #delete user from users table
+        db.session.query(User).filter(User.id==user_id).delete()
+
+        db.session.commit()
+
+        statusText = 'Account successfully deleted'
+    except Exception as e:
+        statusText = 'Account unsuccessfully deleted: ' + str(e)
+        logger.exception(e)
+
+    db.session.close()
+
+    logout_user()
+    return jsonify({'result': statusText})
 
 @api.route('/logout', methods=['GET'])
 @login_required
@@ -67,3 +100,4 @@ def logout():
 @login_required
 def confirm_auth():
     return jsonify({'result': current_user.is_authenticated(), 'user': current_user.get_names()})
+
