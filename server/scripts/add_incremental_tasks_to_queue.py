8cd5e3f09e7e8490d8e7dff2a6f801c92434787b
sarahkemi <sarahaladetan@gmail.com>
2018-01-30 14:02:19 -0500

    new celery task for prioritizing users to update

---
+++ server/scripts/add_incremental_tasks_to_queue.py
@@ -0,0 +1,7 @@
+import tasks as tasks
+
+if __name__ == '__main__':
+    # get post data for some users:
+    tasks.get_posts_data_for_some_users.delay()
+    #should news posts be updated??
+    tasks.get_news_posts.delay()
