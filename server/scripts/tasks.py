dba55615f7da09cfd841129331120dfb1cc48283
Rahul B <rahulbot@gmail.com>
2018-03-22 10:31:24 -0400

    Pull dev/prod out of environment var

--- server/scripts/tasks.py
+++ server/scripts/tasks.py
@@ -71,7 +71,7 @@ def get_posts_data_for_some_users(self):
     time_window = (datetime.now() - User.last_post_fetch) > timedelta(hours=config_map[env].HOURS_TO_WAIT)
 
     #from most recent logins, grab users that haven't had their posts updated in the provided window
-    oldest_post_fetches = recent_logins.filter(User.last_post_fetch is not None).filter(time_window).limit(config_map['prod'].NUMBER_OF_USERS_TO_UPDATE)
+    oldest_post_fetches = recent_logins.filter(User.last_post_fetch is not None).filter(time_window).limit(config_map[env].NUMBER_OF_USERS_TO_UPDATE)
     prioritized_users.extend(oldest_post_fetches.all())
 
     #to fill up the queue, find users who perhaps haven't logged in recently but also haven't had their posts updated in awhile
