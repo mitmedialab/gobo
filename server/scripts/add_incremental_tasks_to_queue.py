fada4067c58e9d4d6bd30a2b90e70ee1fb511acf
Rahul B <rahulbot@gmail.com>
2018-04-22 11:29:50 -0400

    more refactoring on prioritized queueing of users to update

--- server/scripts/add_incremental_tasks_to_queue.py
+++ server/scripts/add_incremental_tasks_to_queue.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime, timedelta
 
@@ -20,42 +20,69 @@ session = Session()
 
 
 def get_posts_data_for_some_users(db_session):
-    logger.info("Searching for users to priorize for updating")
-    #filter for people who haven't updated in awhile but logged in recently
-    prioritized_users = []
-
     env = os.getenv('FLASK_ENV', 'dev')
+    queue_size = config_map[env].NUMBER_OF_USERS_TO_UPDATE
+    prioritized_users = []
+    logger.info("Searching for {} users to priorize for updating".format(queue_size))
 
-    #look for most recent logins (not null)
-    recent_logins = db_session.query(User).filter(User.last_login is not None).order_by(User.last_login.desc())
-    recent_logins_count = recent_logins.count()
-    logger.debug("found {} with recent logins".format(recent_logins_count))
+    # helpers for filtering users
+    connected_to_services = (User.twitter_authorized == True) or (User.facebook_authorized == True)
+    not_fetched_recently = (datetime.now() - User.last_post_fetch) > timedelta(hours=config_map[env].HOURS_TO_WAIT)
 
-    time_window = (datetime.now() - User.last_post_fetch) > timedelta(hours=config_map[env].HOURS_TO_WAIT)
+    # 1. First find connected users that have logged in recently but we haven't updated recently
+    if len(prioritized_users) < queue_size:
+        matching_users = db_session.query(User).\
+            filter(connected_to_services). \
+            filter(User.last_login.isnot(None)). \
+            filter(User.last_post_fetch.isnot(None)).filter(not_fetched_recently). \
+            order_by(User.last_login.desc()). \
+            limit(queue_size - len(prioritized_users))
+        matching_user_count = matching_users.count()
+        logger.debug("  adding {} users that haven't logged in but we haven't updated recently".format(matching_user_count))
+        prioritized_users.extend(matching_users.all())
 
-    #from most recent logins, grab users that haven't had their posts updated in the provided window
-    oldest_post_fetches = recent_logins.filter(User.last_post_fetch is not None).filter(time_window).limit(
-        config_map[env].NUMBER_OF_USERS_TO_UPDATE)
-    prioritized_users.extend(oldest_post_fetches.all())
-    oldest_post_fetches_count = oldest_post_fetches.count()
-    logger.debug("found {} with old post fetches".format(oldest_post_fetches_count))
+    # 2. Then add connected users that we've never updated and haven't logged in a while
+    if len(prioritized_users) < queue_size:
+        matching_users = db_session.query(User).\
+            filter(connected_to_services).\
+            filter(text("last_post_fetch is NULL")).\
+            filter(User.last_login.isnot(None)). \
+            order_by(User.last_login.desc()). \
+            limit(queue_size - len(prioritized_users))
+        matching_user_count = matching_users.count()
+        logger.debug("  adding {} users that haven't logged in recently and we've never updated".format(matching_user_count))
+        prioritized_users.extend(matching_users.all())
 
-    #to fill up the queue, find users who perhaps haven't logged in recently but also haven't had their posts updated in awhile
-    if oldest_post_fetches.all() < config_map[env].NUMBER_OF_USERS_TO_UPDATE:
-        oldest_post_fetches_with_no_login = db_session.query(User).filter(User.last_login is None).filter(time_window).order_by(
-            User.last_post_fetch).limit(config_map[env].NUMBER_OF_USERS_TO_UPDATE - len(oldest_post_fetches))
-        oldest_post_fetches_with_no_login_count = oldest_post_fetches_with_no_login.count()
-        logger.debug("found {} with oldest post fetches but no logins".format(oldest_post_fetches_count))
-        prioritized_users.extend(oldest_post_fetches_with_no_login.all())
+    # 3. And then connected users that we've never updated and haven't logged in
+    if len(prioritized_users) < queue_size:
+        matching_users = db_session.query(User).\
+            filter(connected_to_services).\
+            filter(text("last_login is NULL and last_post_fetch is NULL")).\
+            limit(queue_size - len(prioritized_users))
+        matching_user_count = matching_users.count()
+        logger.debug("  adding {} users that haven't logged in ever and we've never updated".format(matching_user_count))
+        prioritized_users.extend(matching_users.all())
 
-    logger.info("found {} to update".format(len(prioritized_users)))
+    logger.info("found {} to update overall".format(len(prioritized_users)))
 
+    tasks_queued = 0
     for user in prioritized_users:
+        task_queued = False
         if user.twitter_authorized:
             tasks.get_tweets_per_user.delay(user.id)
+            tasks_queued = tasks_queued + 1
+            task_queued = True
         if user.facebook_authorized:
             tasks.get_facebook_posts_per_user.delay(user.id)
-
+            tasks_queued = tasks_queued + 1
+            task_queued = True
+        # and mark that we tried to update them so they move to bottom of the priority list
+        if task_queued:
+            db_session.query(User).filter(User.id == user.id).\
+                update({"last_post_fetch": datetime.now()})
+            logger.info("  Updated user {}".format(user.id))
+    db_session.commit()
+    logger.info("queued {} tasks".format(tasks_queued))
 
 if __name__ == '__main__':
     get_posts_data_for_some_users(session)
