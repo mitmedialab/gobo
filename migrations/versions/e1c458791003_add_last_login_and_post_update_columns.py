9039b1185526869a51d5e8a414fd1b4d7be41991
Rahul B <rahulbot@gmail.com>
2018-08-14 13:42:46 -0400

    safer migrations

--- migrations/versions/e1c458791003_add_last_login_and_post_update_columns.py
+++ migrations/versions/e1c458791003_add_last_login_and_post_update_columns.py
@@ -7,7 +7,7 @@ Create Date: 2018-01-29 23:09:47.273168
 """
 from alembic import op
 import sqlalchemy as sa
-
+import logging
 
 # revision identifiers, used by Alembic.
 revision = 'e1c458791003'
@@ -15,10 +15,17 @@ down_revision = 'f72e54a6c741'
 branch_labels = None
 depends_on = None
 
+logger = logging.getLogger(__name__)
 
 def upgrade():
-    op.add_column('users', sa.Column('last_login', sa.DateTime, nullable=True))
-    op.add_column('users', sa.Column('last_post_fetch', sa.DateTime, nullable=True))
+    try:
+        op.add_column('users', sa.Column('last_login', sa.DateTime, nullable=True))
+    except sa.exc.ProgrammingError as pe:
+        logger.warn("Couldn't add column: users.last_login - {}".format(pe))
+    try:
+        op.add_column('users', sa.Column('last_post_fetch', sa.DateTime, nullable=True))
+    except sa.exc.ProgrammingError as pe:
+        logger.warn("Couldn't add column: users.last_post_fetch - {}".format(pe))
 
 
 def downgrade():
--- migrations/versions/f72e54a6c741_lowercase_emails.py
+++ migrations/versions/f72e54a6c741_lowercase_emails.py
@@ -7,7 +7,7 @@ Create Date: 2018-01-16 12:11:27.615204
 """
 from alembic import op
 import sqlalchemy as sa
-
+import logging
 
 # revision identifiers, used by Alembic.
 revision = 'f72e54a6c741'
@@ -15,9 +15,15 @@ down_revision = None
 branch_labels = None
 depends_on = None
 
+logger = logging.getLogger(__name__)
+
 
 def upgrade():
-    op.execute("UPDATE users SET email = LOWER(email)")
+    try:
+        op.execute("UPDATE users SET email = LOWER(email)")
+    except Exception as e:
+        logger.warn("Couldn't lowercase all emails - {}".format(e))
+
 
 def downgrade():
     pass
