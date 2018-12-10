21e6c1589aedebca60cc84cc5420c0b2cb3e3639
Dennis Jen <dsjen@media.mit.edu>
2018-12-10 14:51:17 -0500

    Added reimported rule

--- server/scripts/gender_classifier/genderComputer/genderComputer.py
+++ server/scripts/gender_classifier/genderComputer/genderComputer.py
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>."""
 
 import re
 import csv
-import os
 from dictUtils import MyDict
 from unicodeMagic import UnicodeReader
 from unidecode import unidecode
