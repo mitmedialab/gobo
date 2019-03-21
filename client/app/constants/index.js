6b14cdc6213e2bf59653c1d4930ccd6020231b61
Dennis Jen <dsjen@cs.unc.edu>
2019-03-21 12:58:11 -0400

    Bump version

--- client/app/constants/index.js
+++ client/app/constants/index.js
@@ -27,6 +27,6 @@ export const DEFAULT_MASTODON_INSTANCES = [
   'mastodon.technology',
 ];
 
-export const VERSION = 'v1.18.0';
+export const VERSION = 'v1.18.1';
 
 export const API_URL = '';  // process.env.NODE_ENV === 'production' ? '': 'http://localhost:5000/'
