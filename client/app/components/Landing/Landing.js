1a90585b35ad0592959dc67ec061e39ed3f16420
Dennis Jen <dsjen@media.mit.edu>
2019-04-10 11:58:31 -0400

    Update landing page copy

--- client/app/components/Landing/Landing.js
+++ client/app/components/Landing/Landing.js
@@ -34,17 +34,17 @@ const Landing = () => (
             />
             <LandingCard
               title="Connect multiple platforms"
-              description="We believe that multiple social media platforms should exist to serve different purposes. However, it’s not easy to keep up with all these platforms, especially when your data can’t be easily shared between them. Gobo allows you to connect up to three platforms, so you can view all of your feeds in one place."
+              description="We believe that multiple social media platforms should exist to serve different purposes. However, it’s not easy to keep up with all of these platforms, especially when your data can’t be easily shared between them. Gobo allows you to connect up to three platforms, so you can view all of your feeds in one place."
               colorClass="card-red"
             />
             <LandingCard
               title="See what gets hidden"
-              description="We believe that transparency can help you better understand what you see on social media and keep platforms accountable for algorithmic bias. Gobo tells you why certain posts are hidden based on the rules you set. It also shows you how many posts are hidden, so you can understand the overall impact of the rules you set."
+              description="We believe that transparency can help you understand what you see on social media and keep platforms accountable for algorithmic bias. Gobo tells you why certain posts are hidden based on the rules you set. It also shows you how many posts are hidden, so you can see how your rules affect your feed."
               colorClass="card-green"
             />
             <LandingCard
               title="Expand your perspective"
-              description="Social media companies make assumptions about what we want to see based on what we read and click on. They tend to show us content we’re already engaging with, reinforcing our echo chambers. Instead of assuming what you want to see, Gobo allows you to add unfamiliar perspectives into your feed, so you can better understand the range of opinions that are shared online."
+              description="Social media companies make assumptions about what we want to see. They tend to show us content we’re already engaging with, reinforcing our echo chambers. Instead of assuming what you want to see, Gobo allows you to add unfamiliar perspectives into your feed, so you can explore the range of opinions that are shared online."
               colorClass="card-purple"
             />
           </div>
