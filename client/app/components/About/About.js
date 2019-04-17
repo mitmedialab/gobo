dd222e9453b56d8b09f5412e6c79413861ff33de
Dennis Jen <dsjen@media.mit.edu>
2019-04-17 16:34:41 -0400

    WIP: added content for new about page

--- client/app/components/About/About.js
+++ client/app/components/About/About.js
@@ -4,85 +4,153 @@ import { Link } from 'react-router-dom';
 import { Button } from '@blueprintjs/core';
 import { connect } from 'react-redux';
 
-import { VERSION } from '../../constants/index';
+import { VERSION } from 'constants/index';
+import COPY from 'constants/featuresCopy';
 
-const propTypes = {
-  auth: PropTypes.object.isRequired,
-};
 
 const About = (props) => {
   let loginContent;
   if (!props.auth.isAuthenticated) {
     loginContent = (
-      <div>
-        <div>
+      <div className="row">
+        <div className="col-md-6">
           <Link to={'/login'}>
-            <Button text={'Login'} />
+            <Button className="button_wide" text={'Login'} />
           </Link>
         </div>
-        <div>
+        <div className="col-md-6">
           <Link to={'/register'}>
-            <Button text={'Register'} />
+            <Button className="button_wide" text={'Register'} />
           </Link>
         </div>
       </div>
     );
   }
   return (
-    <div className="content-with-nav" id="landing-page">
-      <div className="content">
-
-        <h1>Take control of your social feed</h1>
+    <div className="content-with-nav">
+      <div className="container">
         {loginContent}
+        <div className="row">
+          <div className="col-lg-12">
+            <h1>Your social media. Your rules.</h1>
+            <p>
+              Gobo is an experiment, not a startup. We’re building it to change the conversation on social
+              media and imagine a better version of it. This is a technology-to-think-with -- a tool we want
+              you to play with and push against. Gobo is being built by a small team at <a href="https://www.media.mit.edu/groups/civic-media/overview/" target="_blank" rel="noopener noreferrer">MIT Media Lab's Center for Civic Media</a>,
+              where we work on technologies for social change. You can learn more about Gobo and why we built it in our FAQ.
+              For questions, feedback, and musings, you can reach the Gobo team at <a href="mailto:gobo@media.mit.edu">gobo@media.mit.edu</a>.
+            </p>
 
-        <div>
+            <p>{VERSION}</p>
 
-          <h2>Algorithms control what we see on our feeds,<br />but we can't control the algorithms.</h2>
-
-          <p>Do you get most of your news from social media? Us too. Do you know anything about how they pick
-          what to show you? No? Neither do we. Facebook, Twitter, and others sites use complicated computerized
-          rules to decide which posts you see at the top of your feed and which you don't. These
-          algorithms have reinforced our echo chamber - showing us content like what we share - and made
-          hard to burst our filter bubbles - hiding content that is different than what we believe. We
-          think escaping these echo chambers and seeing a wider picture of the news is a critical piece of
-          democratic society. We built Gobo to help demonstrate, and think about, how to do that.</p>
-
-          <h2>Gobo lets you control what you see on your feed,<br />and what gets hidden.</h2>
-
-          <p>Sign up for Gobo, link it to your other social media profiles, and you can take control of your
-          feed. Want to read news you aren't otherwise seeing? Use our "Echo Chamber" filter to see what
-          we call "wider" news. Want a better balance of men and women in your feed? Use our "gender"
-          filter to rebalance it. Want to take a lunch break and just see popular funny videos your friends
-          are sharing? Use our "virality" filter to pick only the most shared content. With Gobo you're in
-          charge of the algorithmic filters that control what you see on social media. We've built a bunch
-          of filters like these already, are building more, and have made it possible for other developers
-          to add filters too. Sign up, try it out, and see if it changes how you think about how social
-          media should work.</p>
-
-          <h2>We want to change how you think social media should work</h2>
-
-          <p>Gobo is an experiment, not a startup. We are building it to change the conversation about social
-          media, algorithms, and feeds. This is a technology-to-think-with; a tool that you should play
-          with and challenge yourself to use. Gobo is being built by a small team of developers at the <a href="https://www.media.mit.edu/groups/civic-media/overview/">MIT Media Lab's Center for Civic
-          Media</a>, where we work on technologies for social change like this. Want to learn more about
-          our motivation? Read our director <a href="https://www.theatlantic.com/technology/archive/2017/05/the-case-for-a-taxpayer-supported-version-of-facebook/524037/">Ethan
-          Zuckerman's piece in the Atlantic</a> for some more background about why we are building Gobo.</p>
-
-          <p>If you have any questions, feedback, or other thoughts you can contact the Gobo team at gobo@media.mit.edu</p>
-
-          <p>{VERSION}</p>
-
+          </div>
         </div>
-      </div>
-      <div className="logos">
-        <a href="https://knightfoundation.org/"><img src="images/KF_logo.png" alt="Knight Foundation" /></a>
-        <a href="https://civic.mit.edu/"><img src="images/CivicMedia_Symbol.jpg" alt="Center for Civic Media" /></a>
+
+        <div className="row">
+          <div className="col-lg-12">
+            <h2>
+              Table of contents
+            </h2>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-12">
+            <h1>Features</h1>
+            {['control', 'connect', 'see', 'perspective'].map(key =>
+              (<div key={key}>
+                <h2>{COPY[key].title}</h2>
+                <p>{COPY[key].description}</p>
+              </div>),
+            )}
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-12">
+            <h1>FAQ</h1>
+
+            <h2>What is Gobo?</h2>
+            <p>Gobo is a social media browser that gives you control and transparency over what you see. Through Gobo, you can connect your Twitter, Facebook, and Mastodon accounts and view all of these feeds in one place. Using a set of “rules”, you can choose to hide certain kinds of posts and show others. For example, our "obscurity" rule allows you to hide viral content that’s overwhelming your feed or surface hidden gems you might be missing. Meanwhile, our "politics" rule allows you to add content from political perspectives you aren’t seeing. Gobo aims to be completely transparent, showing you why each post was included in your feed and inviting you to explore which posts were hidden.</p>
+
+            <h2>Why did we make it?</h2>
+            <p>Social media companies hold a great deal of power over what we see online. Using opaque algorithms, these companies decide what we’re shown on our feeds -- and what we’re not shown. We have little say over how social media should work, and we think this needs to change. It is easier to criticize these companies and their profit motives, but it’s harder to build a viable alternative. We built Gobo to do that -- to demonstrate how we might be able to control social media rather than be controlled by it. What if we could choose to challenge ourselves one day, encountering perspectives different from our own, and relax another day, hiding more serious content? We see Gobo as a speculative tool -- a starting point in thinking about <a href="http://www.ethanzuckerman.com/blog/2018/12/09/we-make-the-media-a-recent-speech-at-freedom-of-speech-online-2018/">social media that serves our best interests.</a></p>
+
+            <h2>How does it work?</h2>
+            <p>Gobo retrieves posts from people you follow on Twitter, Facebook, and Mastodon and analyzes them using <a href="http://perspectiveapi.com/">machine learning APIs</a> and keyword identifiers. You can use most rules  to hide posts from your feed. The politics rules work differently, adding in posts instead of hiding them— if you set the politics slider towards “lots of perspectives”, posts will be added from political news sources that you likely don’t read every day.</p>
+
+            <h2>Who made it?</h2>
+            <p>Gobo is a project of the <a href="https://civic.mit.edu/">Center for Civic Media</a> at the <a href="https://media.mit.edu/">MIT Media Lab</a> and <a href="http://cmsw.mit.edu/">Comparative Media Studies</a> at MIT. Gobo is made possible by the <a href="https://knightfoundation.org/">Knight Foundation</a>, founding donors and longtime supporters of Civic Media, and the <a href="https://aiethicsinitiative.org/">Ethics and Governance of AI Initiative</a>. We’re thankful to both of these organizations and the people behind them!</p>
+
+            <h2>Which platforms does Gobo support?</h2>
+            <p>Currently, you can connect your Twitter, Facebook, and Mastodon accounts. We’re working on supporting more platforms soon!</p>
+
+            <h2>What are rules?</h2>
+            <p>Rules allow you to hide or add certain kinds of posts to your feed.</p>
+
+            <h2>Can I make my own rules?</h2>
+            <p>Technically yes. Gobo is an open source project, so if you have the development chops, you can <a href="https://github.com/mitmedialab/gobo">download our code</a> and add your own rules. However, we’re planning to add an easy option to create your own rules soon!</p>
+
+            <h2>What are the limitations?</h2>
+            <p>You might notice that there’s a lot less content from Facebook than from other platforms. That’s because Facebook allows us to show you posts from public pages you’ve liked, but not from your friends’ individual pages.</p>
+            <p>You might also notice that rules don’t always work the way you’d expect. Some rules use open source machine learning tools while other rules are powered by our own algorithms. It’s a good reminder that these rules are always probabilistic and inexact — we want you to see where our system messes up, so we include information about what tools and datasets we’re using for each rule.</p>
+
+            <h2>What’s our future plan for Gobo?</h2>
+            <p>Major social media platforms do not serve our best interests and are impacting our society in some negative ways. However, we still believe in the core idea of social media -- using the web to be connected with other people. So rather than simply pointing out what the platforms are doing wrong, Gobo is our attempt to design what the future of social media could be. We hope others are encouraged to do the same, so we can move closer to a version of social media that serves us.</p>
+            <p>We’re envisioning a future where it’s easy to navigate between several platforms. That’s why we recently integrated Mastodon, in addition to Twitter and Facebook. We’re experimenting with other platforms and plan to continue adding more!</p>
+            <p>We’re also envisioning social media that’s more personalizable. Right now, you can play with rules we created, but our ultimate goal is for you to create your own rules. Our next step is to release a customizable rules, allowing you to be more specific about the posts you want to hide and add.</p>
+            <p>Finally, we want you to be able to share these rules, so others can get a glimpse into your perspectives.</p>
+            <p>Have any awesome ideas for the future of Gobo? We’d love to hear them at <a href="mailto:gobo@media.mit.edu">gobo@media.mit.edu</a>.</p>
+
+            <h2>What does “Gobo” mean?</h2>
+            <p>Ever seen a stage production where the lights look like they’re coming through a window, or the leaves of a forest? Those effects are created with <a href="https://en.wikipedia.org/wiki/Gobo_%28lighting%29">gobos</a>, filters cut from sheets of metal and placed in front of a light to shine a particular pattern on a curtain or other surface. We’re theater geeks, and it seemed like the perfect name for a product that lets you experiment with the effects of filtering what you see.</p>
+
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-12">
+            <h1>Team</h1>
+            <ul>
+              <li>Ethan Zuckerman, Director</li>
+              <li>Rahul Bhargava, Project Manager</li>
+              <li>Dennis Jen, Lead Developer</li>
+              <li>Anna Chung, Researcher/Designer</li>
+              <li>Alexis Hope, Designer</li>
+              <li>Jasmin Rubinovitz, Past Developer</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-12">
+            <h1>Team</h1>
+            <p>Check out some things that have been written on Gobo!</p>
+            <ul>
+              <li>Anna Chung, <a href="https://onezero.medium.com/how-automated-tools-discriminate-against-black-language-2ac8eab8d6db">How Automated Tools Discriminate Against Black Language</a> (OneZero, Medium, 2/2019)</li>
+              <li>Ethan Zuckerman, <a href="http://www.ethanzuckerman.com/blog/2018/12/09/we-make-the-media-a-recent-speech-at-freedom-of-speech-online-2018/">We Make the Media - a recent speech at Freedom of Speech Online 2018</a> (MIT Media Lab, Medium, 12/2018)</li>
+              <li><a href="https://international.thenewslens.com/article/108378">Interview: MIT’s Ethan Zuckerman Says ‘Be Angry and Engage’</a> (The News Lens, 11/2018)</li>
+              <li>Adam Piore, <a href="https://www.technologyreview.com/s/611826/technologists-are-trying-to-fix-the-filter-bubble-problem-that-tech-helped-create/">Technologists are trying to fix the filter bubble problem that tech helped to create</a> (MIT Technology Review, 8/2018)</li>
+              <li>Ethan Zuckerman, <a href="https://medium.com/trust-media-and-democracy/six-or-seven-things-social-media-can-do-for-democracy-66cee083b91a">Six or seven things social media can do for democracy</a> (Trust, Media, and Democracy; Medium, 5/2018)</li>
+              <li><a href="https://www.npr.org/2018/05/01/607321023/would-we-be-better-off-if-we-didnt-rely-on-1-social-network">Would We Be Better Off If We Didn't Rely On 1 Social Network?</a> (Morning Edition, NPR, 5/2018)</li>
+              <li>Lauren Wamsley, <a href="https://www.npr.org/sections/thetwo-way/2018/05/01/607361849/as-facebook-shows-its-flaws-what-might-a-better-social-network-look-like">As Facebook Shows Its Flaws, What Might A Better Social Network Look Like?</a> (NPR, 5/2018)</li>
+              <li>Tobias Rose-Stockwell, <a href="https://medium.com/s/story/how-to-fix-what-social-media-has-broken-cb0b2737128">How to Design Better Social Media</a> (Social Media, Medium, 4/2018)</li>
+              <li>Rachel Metz, <a href="https://www.technologyreview.com/s/610576/how-to-manipulate-facebook-and-twitter-instead-of-letting-them-manipulate-you/">How to manipulate Facebook and Twitter instead of letting them manipulate you</a> (MIT Technology Review, 3/2018)</li>
+              <li>Rachel Metz, <a href="https://www.technologyreview.com/s/610152/social-networks-are-broken-this-man-wants-to-fix-them/">Social Networks are Broken. This Man Wants to Fix Them</a> (MIT Technology Review, 2/2018)</li>
+              <li>Ethan Zuckerman, <a href="https://www.theatlantic.com/technology/archive/2018/01/facebook-doesnt-care/551684/">Facebook Only Cares About Facebook</a> (The Atlantic, 1/2018)</li>
+              <li>David Talbot, <a href="http://www.bostonmagazine.com/news/2017/11/12/ai-research-boston/">The Robots are Coming</a> (Boston Magazine, 11/2017)</li>
+              <li>Ethan Zuckerman, <a href="https://medium.com/mit-media-lab/who-filters-your-news-why-we-built-gobo-social-bfa6748b5944">Who Filters Your News?” Why we built gobo.social</a> (MIT Media Lab, Medium, 11/2017)</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-About.propTypes = propTypes;
+About.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
 
 function mapStateToProps(state) {
   return {
--- client/app/components/Landing/Landing.js
+++ client/app/components/Landing/Landing.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '@blueprintjs/core';
-
+import COPY from 'constants/featuresCopy';
 import LandingCard from 'components/Landing/LandingCard';
 
 
@@ -31,23 +31,23 @@ const Landing = () => (
         <div className="row">
           <div className="landing-cards">
             <LandingCard
-              title="Control your own feed"
-              description="Social media companies use algorithms to control what we see on our feeds, but we don’t know how these algorithms work.  As a result, we’re often unaware why certain posts show up in our feed while others don’t. Gobo allows you to control the algorithms, or a set of “rules”, so you can decide what gets shown on your feed and know why."
+              title={COPY.control.title}
+              description={COPY.control.description}
               colorClass="card-orange"
             />
             <LandingCard
-              title="Connect multiple platforms"
-              description="We believe that multiple social media platforms should exist to serve different purposes. However, it’s not easy to keep up with all of these platforms, especially when your data can’t be easily shared between them. Gobo allows you to connect up to three platforms, so you can view all of your feeds in one place."
+              title={COPY.connect.title}
+              description={COPY.connect.description}
               colorClass="card-red"
             />
             <LandingCard
-              title="See what gets hidden"
-              description="We believe that transparency can help you understand what you see on social media and keep platforms accountable for algorithmic bias. Gobo tells you why certain posts are hidden based on the rules you set. It also shows you how many posts are hidden, so you can see how your rules affect your feed."
+              title={COPY.see.title}
+              description={COPY.see.description}
               colorClass="card-green"
             />
             <LandingCard
-              title="Expand your perspective"
-              description="Social media companies make assumptions about what we want to see. They tend to show us content we’re already engaging with, reinforcing our echo chambers. Instead of assuming what you want to see, Gobo allows you to add unfamiliar perspectives into your feed, so you can explore the range of opinions that are shared online."
+              title={COPY.perspective.title}
+              description={COPY.perspective.description}
               colorClass="card-purple"
             />
           </div>
