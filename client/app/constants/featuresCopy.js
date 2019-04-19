888f0a85de10354cba6027d72016656773db0fed
Dennis Jen <dsjen@cs.unc.edu>
2019-04-19 09:38:43 -0400

    Added about page (#228)

---
+++ client/app/constants/featuresCopy.js
@@ -0,0 +1,20 @@
+const COPY = {
+  control: {
+    title: 'Control your own feed',
+    description: 'Social media companies use algorithms to control what we see on our feeds, but we don’t know how these algorithms work.  As a result, we’re often unaware why certain posts show up in our feed while others don’t. Gobo allows you to control the algorithms, or a set of “rules”, so you can decide what gets shown on your feed and know why.',
+  },
+  connect: {
+    title: 'Connect multiple platforms',
+    description: 'We believe that multiple social media platforms should exist to serve different purposes. However, it’s not easy to keep up with all of these platforms, especially when your data can’t be easily shared between them. Gobo allows you to connect up to three platforms, so you can view all of your feeds in one place.',
+  },
+  see: {
+    title: 'See what gets hidden',
+    description: 'We believe that transparency can help you understand what you see on social media and keep platforms accountable for algorithmic bias. Gobo tells you why certain posts are hidden based on the rules you set. It also shows you how many posts are hidden, so you can see how your rules affect your feed.',
+  },
+  perspective: {
+    title: 'Expand your perspective',
+    description: 'Social media companies make assumptions about what we want to see. They tend to show us content we’re already engaging with, reinforcing our echo chambers. Instead of assuming what you want to see, Gobo allows you to add unfamiliar perspectives into your feed, so you can explore the range of opinions that are shared online.',
+  },
+};
+
+export default COPY;
