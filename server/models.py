f75d35f9a00385aac97529fa45460b46a6ed2844
Dennis Jen <dsjen@cs.unc.edu>
2019-05-30 09:53:25 -0400

    Revert "Removed dead user model code"

--- server/models.py
+++ server/models.py
@@ -78,6 +78,12 @@ class User(db.Model):
     def is_authenticated(self):
         return True
 
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
     def get_id(self):
         return self.id
 
